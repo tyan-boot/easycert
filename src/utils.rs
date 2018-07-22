9db589b7969636730fc6a5ebfd7c63b088847e40
Tyan Boot <tyanboot@outlook.com>
2018-07-22 21:57:29 +0800

    change HOSTNAME to HOST

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,7 @@ pub fn hostname() -> String {
     if cfg!(windows) {
         env::var("COMPUTERNAME").unwrap()
     } else if cfg!(unix) {
-        env::var("HOSTNAME").unwrap()
+        env::var("HOST").unwrap()
     } else {
         println!("unable to detect platform, use default cn=tyanboot");
         "tyanboot".to_string()
